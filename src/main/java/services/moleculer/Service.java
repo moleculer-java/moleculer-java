4e09926fdfb965d2db230f213707c9b93f9e2a85
Icebob <mereg.norbert@gmail.com>
2017-08-16 10:17:11 +0200

    Remove comments

--- src/main/java/services/moleculer/Service.java
+++ src/main/java/services/moleculer/Service.java
@@ -1,18 +1,11 @@
 package services.moleculer;
 
-/**
- * @author Norbi
- *
- */
 public abstract class Service {
 
 	public Service(String name) {
 		
 	}
 
-	/**
-	 * Lifecycle `created` event
-	 */
 	public void created() {
 	}
 
