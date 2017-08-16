7bb352195194d540690ca347281d8d68df0b55f4
Icebob <mereg.norbert@gmail.com>
2017-08-16 10:11:20 +0200

    Test

--- src/main/java/services/moleculer/Service.java
+++ src/main/java/services/moleculer/Service.java
@@ -1,11 +1,18 @@
 package services.moleculer;
 
+/**
+ * @author Norbi
+ *
+ */
 public abstract class Service {
 
 	public Service(String name) {
 		
 	}
-	
+
+	/**
+	 * Lifecycle `created` event
+	 */
 	public void created() {
 	}
 
