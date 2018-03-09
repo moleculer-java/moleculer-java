37a7cc9a5b7233866fa386fa0e2e34f9081df1d6
Berkes Andras <andras.berkes@mail.com>
2018-03-09 17:54:24 +0100

    Skip middlewares in service loop

--- src/main/java/services/moleculer/config/SpringLoader.java
+++ src/main/java/services/moleculer/config/SpringLoader.java
@@ -28,7 +28,6 @@ package services.moleculer.config;
 import java.util.Map;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -43,24 +42,24 @@ public class SpringLoader implements ApplicationContextAware {
 
 		// Get Service Broker
 		ServiceBroker broker = ctx.getBean(ServiceBroker.class);
-		if (broker == null) {
-			throw new NoSuchBeanDefinitionException(ServiceBroker.class);
-		}
 
 		// Find Middlewares in Spring Application Context
 		Map<String, Middleware> middlewareMap = ctx.getBeansOfType(Middleware.class);
-		if (!middlewareMap.isEmpty()) {
-			Middleware[] middlewares = new Middleware[middlewareMap.size()];
-			middlewareMap.values().toArray(middlewares);
-			broker.use(middlewares);
+		if (middlewareMap != null && !middlewareMap.isEmpty()) {
+			broker.use(middlewareMap.values());
 		}
 
 		// Find Services in Spring Application Context
 		Map<String, Service> serviceMap = ctx.getBeansOfType(Service.class);
-		for (Service service : serviceMap.values()) {
-			broker.createService(service);
+		if (serviceMap != null && !serviceMap.isEmpty()) {
+			for (Service service : serviceMap.values()) {
+				if (service instanceof Middleware) {
+					continue;
+				}
+				broker.createService(service);
+			}
 		}
-
+		
 	}
 
 }
