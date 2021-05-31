a604478b51a1009792035b944a34c4b542bebff4
András Berkes <andras.berkes@mail.com>
2021-05-31 04:08:59 +0200

    V1.2.19

--- src/main/java/services/moleculer/ServiceBroker.java
+++ src/main/java/services/moleculer/ServiceBroker.java
@@ -109,7 +109,7 @@ public class ServiceBroker extends ContextSource implements MetricConstants {
 	/**
 	 * Version of the Java ServiceBroker API.
 	 */
-	public static final String SOFTWARE_VERSION = "1.2.18";
+	public static final String SOFTWARE_VERSION = "1.2.19";
 
 	// --- LOGGER ---
 
